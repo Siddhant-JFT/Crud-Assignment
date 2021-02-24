73ef39e889f2ca81e864279f53fcffafb46f379e
Siddhant-JFT <siddhant.tyagi@jellyfishtechnologies.com>
2021-02-24 12:47:04 +0530

    Updated Crud

--- crud.js
+++ crud.js
@@ -1,7 +1,5 @@
-
 fetched = false
 data = []
-var a;
 
 const load = async () => {
     let result = await fetch('https://jsonplaceholder.typicode.com/users')
@@ -18,6 +16,7 @@ const toAdd = async () => {
         await load();
     }
     // console.log(data)
+    document.getElementById('test1').innerHTML = ""
     let rowTag = document.createElement('div')
     rowTag.classList.add('row')
 
@@ -35,10 +34,10 @@ const toAdd = async () => {
     rowTag.appendChild(nHead)
     rowTag.appendChild(uHead)
     rowTag.appendChild(wHead)
-    let i = 1;
+    counter = 0
     data.forEach((user) => {
-
-        let idDisplay = createData('col-md-1', i++)
+        counter++;
+        let idDisplay = createData('col-md-1', user.id)
         let nameDisplay = createData('col-md-3', user.name)
         let unameDisplay = createData('col-md-3', user.username)
         let websDisplay = createData('col-md-3', user.website)
@@ -46,12 +45,24 @@ const toAdd = async () => {
 
         let editBtn = document.createElement('button')
         editBtn.classList.add('btn', 'btn-primary', 'col-md-1')
-        editBtn.innerHTML = "Edit"
+        editBtn.addEventListener("click", function () {
+            ToEdit(user.id);
+        });
+        editBtn.setAttribute("data-bs-toggle", "modal");
+        editBtn.setAttribute("data-bs-target", "#EditModal");
+        editBtn.innerText = "Edit"
+
+        // let editBtn = "<button class='btn btn-primary col-md-1' data-bs-toggle='modal' data-bs-target='#AddModal'>Edit</button>"
+        // button type="button" class="mx-5 btn btn-primary" data-bs-toggle="modal" >
+        // Add Data
+        // </button >
 
         let delBtn = document.createElement('button');
         delBtn.classList.add('btn', 'btn-danger', 'col-md-1');
+        delBtn.setAttribute("data-bs-toggle", "modal");
+        delBtn.setAttribute("data-bs-target", "#DeleteModal");
         delBtn.addEventListener("click", function () {
-            deleteFunction(user.id)
+            fordel(user.id);
         });
         delBtn.innerText = "Delete";
 
@@ -64,9 +75,9 @@ const toAdd = async () => {
         rowTag.appendChild(document.createElement('hr'))
     })
 
-    document.getElementById('test1').appendChild(rowTag)
-
-
+    document.getElementById('test1').appendChild(rowTag);
+    document.getElementById('count').innerHTML = counter;
+    // counter++;
 }
 
 toAdd();
@@ -97,7 +108,8 @@ function addData() {
         website: document.getElementById("webs").value
     }
     data.push(newObj)
-    // console.log(data)
+    counter++;
+    console.log(data)
     document.getElementById('test1').innerHTML = ''
     toAdd()
     document.getElementById("uname").value = ''
@@ -105,8 +117,39 @@ function addData() {
     document.getElementById("webs").value = ''
 }
 
-function deleteFunction(x) {
-    data.splice((x - 1), 1);
+
+let ind = 0;
+function ToEdit(id) {
+    for (let i = 0; i < data.length; i++) {
+        if (data[i].id === id) {
+            ind = i;
+        }
+
+    }
+    fname = data[ind].name;
+    urname = data[ind].username;
+    webs = data[ind].website;
+
+    document.getElementById("edname").value = data[ind].name;
+    document.getElementById("eduname").value = data[ind].username;
+    document.getElementById("edwebs").value = data[ind].website;
+}
+function editData() {
+    data[ind].name =document.getElementById("edname").value ;
+    data[ind].username= document.getElementById("eduname").value;
+    data[ind].website =document.getElementById("edwebs").value ;
     toAdd();
 }
-// console.log(data)
+
+function DeleteData() {
+    
+    id = document.getElementById('delitem').value;
+    data = data.filter(d => d.id != id);
+    --counter;
+    
+    toAdd();
+}
+
+function fordel(id) {
+    document.getElementById('delitem').value = id;
+}
